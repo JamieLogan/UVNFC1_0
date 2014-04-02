5a62f73096b6079f74d0ae9d19aebaecb20488bd
jaimelogan <jamielogan91@googlemail.com>
2014-04-02 20:18:05 +0100

    end of 2/4/14

--- UVNFC1_0.ino
+++ UVNFC1_0.ino
@@ -1,16 +1,22 @@
 
+#include <Wire.h>
+#include <RF430CL330H_Shield.h>
+#include "Arduino.h"
 
 
 
-#define EEPROM_CLRT 0x50;
 
 
-long int EEPROM_ADR = 0;
-long int MEASCOUNT = 0;
 
-byte TFLAG=0;
-byte NDEFFLAG=1;
-boolean COR_APP=flase;
+byte EEPROM_CTRL = 0x50;
+
+long unsigned int EEPROM_ADR = 0;
+long unsigned int MEASCOUNT = 0;
+int TIMER_NO;
+
+byte TFLAG = 0;
+byte NFCFLAG = 0;
+boolean CORR_APP = false;
 
 
 
@@ -19,7 +25,7 @@ boolean COR_APP=flase;
 void EEPROM_Write(int address, byte data){
   byte add_lo = address;                //split new 16 bit address into high and low bytes
   byte add_hi = (address >> 8);
-  Wire.beginTransmission(EEPROM_CRTL); //start transmission & send control byte
+  Wire.beginTransmission(EEPROM_CTRL); //start transmission & send control byte
   Wire.write(add_hi);                   //send address high byte
   Wire.write(add_lo);                   //send address low byte
   Wire.write(data);                     //send data byte
@@ -30,10 +36,10 @@ void EEPROM_Write(int address, byte data){
 
 /*update the number of measurments stored in the header in the EEPROM*/
 void Update_MeasCount(void){
-    byte meascount[3];
+    byte meascount[3]= {0, 0, 0};
     meascount[0]=MEASCOUNT;         //split the meas conter in to 3 bytes
     meascount[1]=MEASCOUNT>>8;
-    meascount[2]MEASCOUNT>>16
+    meascount[2]=MEASCOUNT>>16;
     EEPROM_Write(9, meascount[0]);  //store the 3 bytes of the meas count in the EEPROM
     EEPROM_Write(8, meascount[1]);
     EEPROM_Write(7, meascount[2]);
@@ -41,7 +47,7 @@ void Update_MeasCount(void){
 
 
 /*take a measurment from the sensors and store it in the EEPROM and update the meas count in the data header in EEPROM*/
-void Take_Store_Meas(Void){
+void Take_Store_Meas(void){
   MEASCOUNT++;                    //incrament the number of measurments taken
   int uv = analogRead(A0);        //read and store the uv value
   EEPROM_Write(EEPROM_ADR, uv);
@@ -49,7 +55,7 @@ void Take_Store_Meas(Void){
   int amb = analogRead(A1);        //read and store the ambiant value
   EEPROM_Write(EEPROM_ADR, amb);
   EEPROM_ADR++;
-  update_MeasCount()               //update the number of measurments taken in EEPROM
+  Update_MeasCount();              //update the number of measurments taken in EEPROM
 }
 
 
@@ -58,22 +64,19 @@ void Stop_Meas(void){
  //disable timer interupts 
 }
 
-void Copy_Header_To_Memory(void){
-  
-}
+
 
 
 /*this function compares two arrays and returns whither or not they are the sane as an boolean*/
-boolean array_cmp(int *a, int *b){
+boolean array_cmp(int *a, int *b, int len){
       int n;
-      for (n=0;n<len_a;n++) if (a[n]!=b[n]) return false;    // test each element to be the same. if not, return false
+      for (n=0;n<len;n++) if (a[n]!=b[n]) return false;    // test each element to be the same. if not, return false
       return true;                                           //ok, if we have not returned yet, they are equal
 }
 
 
 /* this function checks if the NDEF data is from the correct app and returns the NDEF data as a byte array*/
-byte[] Get_NDEF_Data(Void){
-    byte buffer[1024];
+void Get_NDEF_Data(byte *buffer){
     byte R_mime[27];
     int x;
     
@@ -83,48 +86,50 @@ byte[] Get_NDEF_Data(Void){
     Tag4Length = NDEFMessageLength + 0x1C;                 // entire Tag length  
     nfc.Read_Continuous(0, buffer, Tag4Length);            //get the entire tag data
     
-    for(x=0x1F, x<=0x39, x++){                             //extrace AAR from message
-      R_mime[x-0x1F]=buffer[x]
+    for(x=0x1F; x<=0x39; x++){                             //extrace AAR from message
+      R_mime[x-0x1F]=buffer[x];
     }
-    if (array_cmp(R_mime, MIME_TYPE) == true){             //check if message is from correct app
-            COR_APP=true;
+    if (array_cmp(&R_mime, &MIME_TYPE, 27) == true){             //check if message is from correct app
+            CORR_APP=true;
       }else{
             CORR_APP= false;
       }
-    return buffer[];
 }
   
 /*this function copys data from the nedf message to the header in EEPROM*/
-void Copy_Header_To_Memory(Byte data[]){
+void Copy_Header_To_Memory(byte data[1024]){
+  int i;
   for(i=0; i<6; i++){            //copy the data from the phone to the EEPROM (Rory thinks the payload starts at byte 63 may be 62)
-    EEPROM_Write(x, data[x+63];
+    EEPROM_Write(i, data[i+63]);
   }
   for (i=6; i<9; i++){           //set the 3 bytes of the mesurment counter in EEPROM to zero
-    EEPROM_Write(x, 0);
+    EEPROM_Write(1, 0);
   }
 }
 
 
 void loop (void){
   
-  if(TFLAG==1){
-    TFLAG=0;
-    take_store_meas();
-    Update_NDEF();
+  if(TFLAG==1){                             //on timer interupt
+    TFLAG=0;                                //reset the timer interupt flag
+    Take_Store_Meas();                      //make a measurment and store it in the EEPROM
+    //Update_NDEF();                          //update the NDEF registor
   }
-  if(NDEFFLAG==1){
-    NDEFFLAG=0;
-    if(READ){
-      Stop_Meas();
+  if(NFCFLAG==1){                          //on NFC interupt
+    NFCFLAG=0;                             //reset the NFC flag
+    if(flags & EOR_INT_FLAG){               //if the phone sucks data
+      Stop_Meas();                          //stop makeing measurments
     }
-    if(WRITE){
-      byte data[]=Get_NDEF_Data();
-      if(CORR_APP==true){
-        copy_header_to_memory(data[]);
-        EEPROM_ADR=10;
-        timer_no= (data[69]*10);
+    if(flags & EOW_INT_FLAG){               //if the phone sends data to the device
+      byte data[1024];
+      Get_NDEF_Data(&data);          //get the NDEF data and check it is from the correct app
+      if(CORR_APP==true){                   //if correct app
+        Copy_Header_To_Memory(data[]);      //copy the NDEF data into the header in EEPROM
+        EEPROM_ADR=10;                      //set the EEPROM pointer to after the header
+        TIMER_NO= (data[69]*10);            //setup the number of interupts between mesurments
       }
     }
+  }
 }
       
   
